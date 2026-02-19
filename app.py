e13ddd0e22a8dde5afddd983c5c0260e5fc6c29a
Alperen KILIÇ <kilicalperen80@gmail.com>
2026-02-19 16:08:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,9 @@ if not os.path.exists(OUTPUT_DIR):
 @app.route('/')
 def home():
     return render_template('index.html')
+@app.route('/favicon.png')
+def favicon():
+    return send_file(os.path.join(BASE_DIR, 'favicon.png'), mimetype='image/png')
 
 # ══════════════════════════════════════════════════════════════
 #  LOGIC: GHOST MODE + PROVIDER KILLER + JSON INJECTOR
